016101740ee86c34ff3dbf2d8e1d03aa57d178f7
Manu1ND <manjunath2000@hotmail.com>
2020-06-14 03:18:06 +0530

    minor

--- user/home.php
+++ user/home.php
@@ -63,29 +63,6 @@ include("session.php");
 					</div>
 					<!-- Button trigger modal -->
 					<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#staticBackdrop">
-						For Attendance click here
-					</button>
-					<!-- Modal -->
-					<div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-						<div class="modal-dialog">
-							<div class="modal-content">
-								<div class="modal-header">
-									<h5 class="modal-title" id="staticBackdropLabel">Attendance</h5>
-									<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-										<span aria-hidden="true">&times;</span>
-									</button>
-								</div>
-								<div class="modal-body">
-									<?= $_SESSION['name'] ?>, click the below button for attendance.
-								</div>
-								<div class="modal-footer">
-									<button type="button" class="btn btn-primary">Attendance</button>
-								</div>
-							</div>
-						</div>
-					</div>
-					<!-- Button trigger modal -->
-					<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#staticBackdrop">
   					For Attendance click here
 					</button>
 					<!-- Modal -->
@@ -99,7 +76,7 @@ include("session.php");
         							</button>
       							</div>
       							<div class="modal-body">
-        							Click the below button for attendance
+								  <?= $_SESSION['name'] ?>, click the below button for attendance
       							</div>
       							<div class="modal-footer">
         							<button type="button" class="btn btn-primary">Attendance</button>
